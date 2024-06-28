ad85b5798c5eb29eeb754b38734279cf7071bc3e
FELIPE ADRIANO WISBECK <felipe_wisbeck@estudante.sc.senai.br>
2024-06-27 21:15:58 -0300

    Tela de Login de Admin Finalizadas

--- Front-end/src/components/admin/loginADM.vue
+++ Front-end/src/components/admin/loginADM.vue
@@ -1,42 +1,48 @@
 <template>
   <v-container fluid class="login-container fill-height">
-      <v-row style="margin: 0;" justify="center">
-        <v-col cols="12" sm="8" md="6">
-          <div class="logo-container text-center">
-            <img class="carreiras-logo" src="/src/assets/Logo_branco.png" alt="">
-          </div>
-          <v-card class="elevation-12" rounded="xl" >
-            <v-card-text>
-              <h1 class="text-center my-1">Login Admin</h1>
-              <v-form class="my-3"  @submit.prevent>
-                <v-text-field
-                  v-model="email"
-                  :rules="emailRules"
-                  label="E-mail"
-                  class="mb-4"
-                  bg-color="#F7F7F7"
-                ></v-text-field>
-                <v-text-field
-                  v-model="password"
-                  :rules="passwordRules"
-                  label="Senha"
-                  class="mb-4"
-                  bg-color="#F7F7F7"
-                ></v-text-field>
-                <div class="sign-in-buttons d-flex justify-center">
-                  <v-btn lass="adm-btn" class="bg-purple-darken-4 me-2">Entrar</v-btn>
-                  <v-btn class="adm-btn " variant="outlined">Cadastre-se</v-btn>
-                </div>
-                <div class="api-google">
-                  <br>
-                  <!--Usar aqui a api-->
-                  <br>
-                </div>
-              </v-form>
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
+    <v-row style="margin: 0" justify="center">
+      <v-col cols="12" sm="8" md="6">
+        <div class="logo-container text-center">
+          <img
+            class="carreiras-logo"
+            src="/src/assets/Logo_branco.png"
+            alt=""
+          />
+        </div>
+        <v-card class="elevation-12" rounded="xl">
+          <v-card-text>
+            <h1 class="text-center my-1">Login de Administrador</h1>
+            <v-form class="my-3" @submit.prevent>
+              <v-text-field
+                v-model="usuario"
+                :rules="usuarioRules"
+                label="Usuário"
+                class="mb-4"
+                bg-color="#F7F7F7"
+              ></v-text-field>
+              <v-text-field
+                v-model="password"
+                :rules="passwordRules"
+                label="Senha"
+                class="mb-4"
+                bg-color="#F7F7F7"
+              ></v-text-field>
+              <div class="sign-in-buttons d-flex justify-center">
+                <v-btn lass="adm-btn" class="bg-purple-darken-4 me-2"
+                  >Entrar</v-btn
+                >
+                <v-btn class="adm-btn" variant="outlined">Voltar</v-btn>
+              </div>
+              <div class="api-google">
+                <br />
+                <!--Usar aqui a api-->
+                <br />
+              </div>
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -44,13 +50,12 @@
 export default {
   data() {
     return {
-      email: '',
-      emailRules: [
-        (v) => !!v || 'E-mail requerido',
-        (v) => /.+@.+\..+/.test(v) || 'E-mail deve ser válido',
+      usuario: "",
+      usuarioRules: [
+        "Usuário requerido",
       ],
-      password: '',
-      passwordRules: [(v) => !!v || 'Senha requerida'],
+      password: "",
+      passwordRules: [(v) => !!v || "Senha requerida"],
     };
   },
 };
@@ -58,7 +63,7 @@ export default {
 
 <style lang="scss" scoped>
 .login-container {
-  background-color: #6832D2 !important;
+  background-color: #6832d2 !important;
   height: 100% !important;
 }
 
@@ -75,7 +80,7 @@ export default {
   flex-direction: column;
   align-items: center;
 }
- 
+
 .carreiras-logo {
   height: 70px;
   width: fit-content;
@@ -87,7 +92,7 @@ export default {
 }
 
 .adm-btn {
-  border: thin solid #3a1c76!important;
-  color: #3a1c76!important;
+  border: thin solid #3a1c76 !important;
+  color: #3a1c76 !important;
 }
 </style>
