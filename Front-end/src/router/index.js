da27e921fe9862e6d19102705516176a2c17835b
FELIPE ADRIANO WISBECK <felipe_wisbeck@estudante.sc.senai.br>
2024-07-30 20:32:28 -0300

    Fazendo correções no codigo

--- Front-end/src/router/index.js
+++ Front-end/src/router/index.js
@@ -8,20 +8,21 @@ import EDCcandidato from '@/components/edicao/EDCcandidato.vue';
 import PerfilCandidato from '@/pages/perfilCandidato.vue';
 import AgendaCandidato from '@/pages/AgendaCandidato.vue';
 import ListaCandidatosEmpresa from '@/pages/ListaCandidatosEmpresa.vue';
+import ListaEmpresas from '@/pages/ListaEmpresas.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', component: Index }, // Página Index como a rota padrão
-    { path: '/login', component: Login},
-    { path: '/cadastro-empresa', component: CADempresa},
-    { path: '/cadastro-candidato', component: CADcandidato},
-    { path: '/empresa-candidato', component: EntrarEmpresaOuCandidato},
-    { path: '/editar-candidato', component: EDCcandidato},
-    { path: '/perfil-candidato',component: PerfilCandidato},
-    { path: '/agenda-candidato', component: AgendaCandidato},
-    { path: '/lista-candidatos-empresa', component: ListaCandidatosEmpresa},
-    {path: '/lista-empresas', components: ListaEmpresas}
+    { path: '/login', component: Login },
+    { path: '/cadastro-empresa', component: CADempresa },
+    { path: '/cadastro-candidato', component: CADcandidato },
+    { path: '/empresa-candidato', component: EntrarEmpresaOuCandidato },
+    { path: '/editar-candidato', component: EDCcandidato },
+    { path: '/perfil-candidato', component: PerfilCandidato },
+    { path: '/agenda-candidato', component: AgendaCandidato },
+    { path: '/lista-candidatos-empresa', component: ListaCandidatosEmpresa },
+    { path: '/lista-empresas', component: ListaEmpresas }
   ],
 });
 
