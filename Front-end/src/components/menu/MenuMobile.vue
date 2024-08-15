371b9c36ee9087eb5aa5047657d48de236d1fee9
Thiago <mauesckt@gmail.com>
2024-08-14 21:17:17 -0300

    atu

--- Front-end/src/components/menu/MenuMobile.vue
+++ Front-end/src/components/menu/MenuMobile.vue
@@ -11,11 +11,11 @@
             <v-btn icon="mdi-chevron-left" @click="dialog = !dialog"></v-btn>
         </div>
         <v-list nav class="bg-deep-purple-accent-3">
-            <v-divider class="mb-4"></v-divider>
+            <v-divider class="ma-4 mt-0 border-opacity-100 "></v-divider>
             <v-list-item prepend-icon="mdi-home" title="Home" value="home" @click="redirectToHome"></v-list-item>
             <v-list-item prepend-icon="mdi-book-open" title="Sobre o Carreiras" value="sobre"></v-list-item>
             <v-list-item prepend-icon="mdi-phone" title="Contate-nos" value="contato"></v-list-item>
-            <v-divider class="mt-4 mb-4"></v-divider>
+            <v-divider class="ma-4 border-opacity-100"></v-divider>
             <v-list-item prepend-icon="mdi-shield-account" title="Adm" value="adm"></v-list-item>
             <v-list-item prepend-icon="mdi-account-plus" title="Cadastro" value="cadastro" @click="redirectToCad"></v-list-item>
             <v-list-item prepend-icon="mdi-login" title="Login" value="login" @click="redirectToLogin"></v-list-item>
