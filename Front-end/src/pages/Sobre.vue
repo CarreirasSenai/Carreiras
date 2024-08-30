a6ccea15a9c22340c08c3bc3b9980ace8c4b7020
FELIPE ADRIANO WISBECK <felipe_wisbeck@estudante.sc.senai.br>
2024-08-29 21:28:09 -0300

    Comecando a tela sobre nos, Contate-nos e o footer

---
+++ Front-end/src/pages/Sobre.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="sobre-nos">
+    <h1>Sobre Nós</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet
+      nulla auctor, vestibulum magna sed, convallis ex.
+    </p>
+    <ul>
+      <li>
+        <strong>Missão:</strong> Desenvolver soluções inovadoras e eficazes para
+        nossos clientes.
+      </li>
+      <li>
+        <strong>Visão:</strong> Ser líderes no mercado de tecnologia e inovação.
+      </li>
+      <li>
+        <strong>Valores:</strong> Inovação, qualidade, respeito e compromisso.
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Sobre from '@/pages/Sobre.vue';
+
+export default {
+  components: { Sobre },
+};
+</script>
+
+<style scoped>
+.sobre-nos {
+  max-width: 800px;
+  margin: 40px auto;
+  text-align: center;
+}
+
+.sobre-nos h1 {
+  font-size: 36px;
+  margin-bottom: 20px;
+}
+
+.sobre-nos p {
+  font-size: 18px;
+  margin-bottom: 40px;
+}
+
+.sobre-nos ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+.sobre-nos li {
+  margin-bottom: 20px;
+}
+
+.sobre-nos li strong {
+  font-weight: bold;
+}
+</style>
--- Front-end/src/router/index.js
+++ Front-end/src/router/index.js
@@ -15,6 +15,7 @@ import ListaEmpresas from '@/components/admin/ListaEmpresas.vue';
 import loginADM from '@/components/admin/loginADM.vue';
 import ListaDeUsuarios from '@/components/admin/ListaDeUsuarios.vue';
 import PerfilEmpresa from '@/pages/perfilEmpresa.vue';
+//import Sobre from '@/pages/Sobre.vue'; 
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -42,6 +43,7 @@ const router = createRouter({
     { path: '/lista-empresas', component: ListaEmpresas },
     { path: '/login-admin', component: loginADM },
     { path: '/lista-de-usuarios', component: ListaDeUsuarios },
+   // { path: '/sobre', component: Sobre },
   ],
 });
 
