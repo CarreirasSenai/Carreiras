3caa20286cd22fabbacb4f78827fb21ae513d1b6
Paula <paularossini96@gmail.com>
2024-09-02 20:55:21 -0300

    Mudança de porta do banco + aparencia do perfil

--- Front-end/src/components/perfis/candidato/InformacoesPessoais.vue
+++ Front-end/src/components/perfis/candidato/InformacoesPessoais.vue
@@ -1,44 +1,18 @@
 <template>
     <v-card>
-        <v-card-title class="cor-primaria" title="teste">Informações Pessoais</v-card-title>
+        <v-card-title class="cor-primaria">Informações Pessoais</v-card-title>
         <v-card-text class="d-flex">
             <v-container>
                 <v-row>
-                    <v-col cols="6" md="2" class="infos-pessoais">
-                        <p>Nome:</p>
-                        <p>Sobrenome:<v-tooltip activator="parent" location="right">Sobrenome</v-tooltip></p>
-                        <p>E-mail:</p>
-                        <p>Telefone:<v-tooltip activator="parent" location="right">Telefone</v-tooltip></p>
-                        <p>Celular:</p>
-                        <p>CPF:</p>
-                        <p>Senha:</p>
-                    </v-col>
-                    <v-col cols="6" md="4" class="infos-valores">
-                        <p>{{ nome }}<v-tooltip activator="parent" location="right">{{ nome }}</v-tooltip></p>
-                        <p>{{ sobrenome }}<v-tooltip activator="parent" location="right">{{ sobrenome }}</v-tooltip></p>
-                        <p>{{ email }}<v-tooltip activator="parent" location="right">{{ email }}</v-tooltip></p>
+                    <v-col cols="6" md="6" class="infos-pessoais">
+                        <p>{{ nome }}&nbsp;{{ sobrenome }}<v-tooltip activator="parent" location="right">{{ nome }}&nbsp;{{ sobrenome }}</v-tooltip></p>
                         <p>{{ telefone }}<v-tooltip activator="parent" location="right">{{ telefone }}</v-tooltip></p>
                         <p>{{ celular }}<v-tooltip activator="parent" location="right">{{ celular }}</v-tooltip></p>
-                        <p>{{ cpf }}<v-tooltip activator="parent" location="right">{{ cpf }}</v-tooltip></p>
-                        <p>{{ senha }}<v-tooltip activator="parent" location="right">{{ senha }}</v-tooltip></p>
                     </v-col>
-                    <v-col cols="6" md="2" class="infos-pessoais">
-                        <p>CEP:</p>
-                        <p>Rua:</p>
-                        <p>Número:</p>
-                        <p>Complemento:<v-tooltip activator="parent" location="right">Complemento</v-tooltip></p>
-                        <p>Bairro:</p>
-                        <p>Cidade:</p>
-                        <p>Estado:</p>
-                    </v-col>
-                    <v-col cols="6" md="3" class="infos-valores">
-                        <p>{{ cep }}<v-tooltip activator="parent" location="right">{{ cep }}</v-tooltip></p>
-                        <p>{{ rua }}<v-tooltip activator="parent" location="right">{{ rua }}</v-tooltip></p>
-                        <p>{{ numero }}<v-tooltip activator="parent" location="right">{{ numero }}</v-tooltip></p>
-                        <p>{{ complemento }}<v-tooltip activator="parent" location="right">{{ complemento }}</v-tooltip></p>
-                        <p>{{ bairro }}<v-tooltip activator="parent" location="right">{{ bairro }}</v-tooltip></p>
-                        <p>{{ cidade }}<v-tooltip activator="parent" location="right">{{ cidade }}</v-tooltip></p>
-                        <p>{{ estado }}<v-tooltip activator="parent" location="right">{{ estado }}</v-tooltip></p>
+                    <v-col cols="6" md="6" class="infos-pessoais">
+                        <p>{{ email }}<v-tooltip activator="parent" location="right">{{ email }}</v-tooltip></p>
+                        <p>{{ cidade }} - {{ estado }}</p>
+                        <p>CEP: {{ cep }}</p>
                     </v-col>
                 </v-row>
             </v-container>
@@ -68,12 +42,8 @@ export default {
 </script>
 
 <style scoped>
-* {
-    /* border: 1px solid red; */
-}
-
 p {
-    margin: 5px;
+    margin: 5.8px;
     overflow: hidden;
     white-space: nowrap;
     text-overflow: ellipsis;
@@ -82,8 +52,4 @@ p {
 p title {
     color: red;
 }
-
-/* .infos-pessoais p {
-    color: #6732d2;
-} */
 </style>
